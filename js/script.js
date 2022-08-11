5f2ad28a5116253d620a5ea5550c11b434a86b89
suhanimall <76207467+suhanimall@users.noreply.github.com>
2022-08-11 19:22:57 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// Question 1
+const targetdiv = document.getElementById("answer1");
+const btn = document.getElementById("question1");
+btn.onclick = function(){
+    if(targetdiv.style.display !== "none")
+    {
+        targetdiv.style.display = "none";
+    }
+    else{
+        targetdiv.style.display = "block";
+    }
+}
+
+// Question 2
+const question2 = document.getElementById("answer2");
+const btn2 = document.getElementById("question2");
+btn2.onclick = function(){
+    if(question2.style.display !== "none")
+    {
+        question2.style.display = "none";
+    }
+    else{
+        question2.style.display = "block";
+    }
+}
+
+// Question 3
+const question3 = document.getElementById("answer3");
+const btn3 = document.getElementById("question3");
+btn3.onclick = function(){
+    if(question3.style.display !== "none")
+    {
+        question3.style.display = "none";
+    }
+    else{
+        question3.style.display = "block";
+    }
+}
+
+// Question 4
+const question4 = document.getElementById("answer4");
+const btn4 = document.getElementById("question4");
+btn4.onclick = function(){
+    if(question4.style.display !== "none")
+    {
+        question4.style.display = "none";
+    }
+    else{
+        question4.style.display = "block";
+    }
+}
+
+// Question 5
+const question5 = document.getElementById("answer5");
+const btn5 = document.getElementById("question5");
+btn5.onclick = function(){
+    if(question5.style.display !== "none")
+    {
+        question5.style.display = "none";
+    }
+    else{
+        question5.style.display = "block";
+    }
+}
+
+// Question 6
+const question6 = document.getElementById("answer6");
+const btn6 = document.getElementById("question6");
+btn6.onclick = function(){
+    if(question6.style.display !== "none")
+    {
+        question6.style.display = "none";
+    }
+    else{
+        question6.style.display = "block";
+    }
+}
